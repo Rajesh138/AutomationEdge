70e26f400def27ced69cb1eaad793bc7382995d7
tejaswinig14 <tejaswinigawande14@gmail.com>
2019-07-29 12:40:44 +0530

    removed package

--- com.automation.selenium/src/main/java/com/automation/aecommonfunctions/workflowAssignmentPage.java
+++ com.automation.selenium/src/main/java/com/automation/aecommonfunctions/workflowAssignmentPage.java
@@ -1,8 +1,6 @@
 package com.automation.aecommonfunctions;
 
-import javax.management.DescriptorKey;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
